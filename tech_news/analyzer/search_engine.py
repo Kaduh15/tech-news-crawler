995ab0544ccfe9784480dcf34da25a7b25ac91a6
kaduh15 <kadu.siva2014@gmail.com>
2023-02-09 11:02:12 -0300

    feat(req08): implementa a função search_by_date ✨

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from tech_news.database import find_news
+# from tech_news.scraper import get_tech_news
 
 
 def search_advanced_news(search: str, content: str):
@@ -19,9 +21,24 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    list_news = find_news()
+    if len(date.split('-')[-1]) == 4:
+        format_date = datetime.strptime(date, '%d-%m-%Y')
+    else:
+        format_date = datetime.strptime(date, '%Y-%m-%d')
+
+    news_filtered = []
+    for news in list_news:
+        date_news = datetime.strptime(news['timestamp'], '%d/%m/%Y').date()
+        if format_date == date_news:
+            news_filtered.append((news['title'], news['url']))
+    return news_filtered
 
 
 # Requisito 9
 def search_by_category(category):
     return search_advanced_news("category", category)
+
+
+if __name__ == '__main__':
+    search_by_date("03-02-2023")
