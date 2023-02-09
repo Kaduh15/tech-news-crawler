4d25ae2a8617b6f56714b90e7368636d295ddfda
kaduh15 <kadu.siva2014@gmail.com>
2023-02-08 21:18:19 -0300

    feat(req07): implementa a função search_by_title ✨

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_data = find_news()
+    news_filtered = []
+    for news in news_data:
+        title_news = news["title"].lower()
+        has_title = title.lower() in title_news
+        if has_title:
+            news_filtered.append((news['title'], news['url']))
+    return news_filtered
 
 
 # Requisito 8
