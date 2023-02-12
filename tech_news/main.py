612f52fc43ffff525d313e6ad59a83c852be8dab
kaduh15 <kadu.siva2014@gmail.com>
2023-02-12 11:57:14 -0300

    feat(main): pesiste a aplicação até que o usuario queira sair ✨

--- tech_news/main.py
+++ tech_news/main.py
@@ -1,5 +1,17 @@
+import os
+from rich import print
 from tech_news.menu import analyzer_menu
+from tech_news.color_terminal import color_green as cg, color_red as cr
 
 
 if __name__ == "__main__":
-    analyzer_menu()
+    while True:
+        option, func = analyzer_menu()
+        os.system('clear') or None
+        print(func())
+        if option == "5":
+            break
+        continue_ = input(f'\nDeseja continuar? [{cg("s")}/{cr("n")}]: ')
+        os.system('clear') or None
+        if continue_.lower() == "n":
+            break
