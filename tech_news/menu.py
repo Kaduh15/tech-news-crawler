b840f6a19a04d491139d3582f72f50dd552b97e4
kaduh15 <kadu.siva2014@gmail.com>
2023-02-12 10:54:44 -0300

    refactor: muda a logica do validação da opção selecionada ♻

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,16 +1,20 @@
-import sys
+import os
 from tech_news.analyzer.ratings import top_5_categories
 from tech_news.analyzer.search_engine import (
     search_by_category,
     search_by_date,
     search_by_title,
 )
+from tech_news.color_terminal import color_red as cr, color_yellow as cy
 from tech_news.scraper import get_tech_news
 
 
 def popula_banco_de_dados():
     amount = int(input("Digite quantas notícias serão buscadas: "))
+    os.system('clear') or None
+    print(f'buscando todas as {amount} notícias, aguarde...')
     get_tech_news(amount)
+    print('Busca Finalizada')
 
 
 def busca_por_titulo():
@@ -40,12 +44,12 @@ def print_saida():
 
 
 OPTIONS = {
-    0: popula_banco_de_dados,
-    1: busca_por_titulo,
-    2: busca_por_data,
-    3: busca_por_categoria,
-    4: top_5,
-    5: print_saida,
+    '0': popula_banco_de_dados,
+    '1': busca_por_titulo,
+    '2': busca_por_data,
+    '3': busca_por_categoria,
+    '4': top_5,
+    '5': print_saida,
 }
 
 
@@ -58,14 +62,14 @@ def analyzer_menu():
         " 2 - Buscar notícias por data;\n"
         " 3 - Buscar notícias por categoria;\n"
         " 4 - Listar top 5 categorias;\n"
-        " 5 - Sair.",
+        " 5 - Sair.\n"
+        "Digite um valor entre 0 e 5: "
     )
-    try:
-        option = int(option)
-        print(OPTIONS[option]())
-    except Exception:
-        return print("Opção inválida", file=sys.stderr)
 
+    while not (option in OPTIONS):
+        print(cr('Opção inválida'))
+        option = input(
+            f'Digite um valor entre {cy("0")} e {cy("5")}: '
+        )
 
-if __name__ == "__main__":
-    analyzer_menu()
+    return OPTIONS[int(option)]()
